ec1f56ec9b4480642ab35ad4a38a2e2d5b1bce0c
Cameron Grieselding <cameron@Camerons-MacBook-Pro.local>
2021-08-11 19:25:14 -0400

    update push on Aug11

--- public/script.js
+++ public/script.js
@@ -1,10 +1,19 @@
 const baseURL = "https://calendarific.com/api/v2/holidays";
 const key = "773023fb9769ee94ac0bda6b6127eba64e5ee283";
 const url = baseURL + "?&api_key=" + key + "&country=DE&year=2021";
-// const results = document.getElementById('results')
 
-const tableHeader = document.getElementById("tableHeader");
-const holidayNames = document.querySelector("tbody");
+const januaryTable = document.getElementById('january');
+const februaryTable = document.getElementById('february');
+const marchTable = document.getElementById('march');
+const aprilTable = document.getElementById('april');
+const mayTable = document.getElementById('may');
+const juneTable = document.getElementById('june');
+const julyTable = document.getElementById('july');
+const augustTable = document.getElementById('august');
+const septemberTable = document.getElementById('september');
+const octoberTable = document.getElementById('october');
+const novemberTable = document.getElementById('november');
+const decemberTable = document.getElementById('december');
 
 function germanHolidays() {
   fetch(url)
@@ -18,31 +27,230 @@ function germanHolidays() {
 germanHolidays();
 
 function displayGermanHolidays(data) {
+
   for (holidays of data.response.holidays) {
     console.log(holidays.name);
 
-    const januaryRow = document.createElement("tr");
-    const januaryHolidays = document.createElement("td");
-
-    const februaryRow = document.createElement('tr');
-    const februaryHolidays = document.createElement('td');
-
-
-
-
+    const allRow = document.createElement('tr');
+    const allHolidaysInfo = document.createElement('td');
+    const allDateInfo = document.createElement('td');
+    const allDescriptInfo = document.createElement('td');
+    
     if (holidays.date.datetime.month == 1) {
-
-      januaryHolidays.innerText = holidays.name;
-
-      januaryRow.appendChild(januaryHolidays);
-      holidayNames.appendChild(januaryRow);
+      
+      allHolidaysInfo.innerText = holidays.name;
+      allDateInfo.innerText = holidays.date.datetime.month + "/" + holidays.date.datetime.day + "/" + holidays.date.datetime.year
+      allDescriptInfo.innerText = holidays.description;
+      
+      const holidayType = holidays.type;
+      const combineHolidayType = holidayType.join(', ');
+      
+      const allTypeInfo = document.createElement('td');
+      allTypeInfo.innerText = combineHolidayType;
+      
+      januaryTable.appendChild(allRow);
+      allRow.appendChild(allHolidaysInfo);
+      allRow.appendChild(allDateInfo);
+      allRow.appendChild(allTypeInfo);
+      allRow.appendChild(allDescriptInfo);
 
     } else if (holidays.date.datetime.month == 2) {
 
-      februaryHolidays.innerText = holidays.name;
+      allHolidaysInfo.innerText = holidays.name;
+      allDateInfo.innerText = holidays.date.datetime.month + "/" + holidays.date.datetime.day + "/" + holidays.date.datetime.year
+      allDescriptInfo.innerText = holidays.description;
 
-      februaryRow.appendChild(februaryHolidays);
-      holidayNames.appendChild(februaryRow);
+      const holidayType = holidays.type;
+      const combineHolidayType = holidayType.join(', ');
+      
+      const allTypeInfo = document.createElement('td');
+      allTypeInfo.innerText = combineHolidayType;
+
+      februaryTable.appendChild(allRow);
+      allRow.appendChild(allHolidaysInfo);
+      allRow.appendChild(allDateInfo);
+      allRow.appendChild(allTypeInfo);
+      allRow.appendChild(allDescriptInfo);
+
+    } else if (holidays.date.datetime.month == 3) {
+
+      allHolidaysInfo.innerText = holidays.name;
+      allDateInfo.innerText = holidays.date.datetime.month + "/" + holidays.date.datetime.day + "/" + holidays.date.datetime.year
+      allDescriptInfo.innerText = holidays.description;
+
+      const holidayType = holidays.type;
+      const combineHolidayType = holidayType.join(', ');
+      
+      const allTypeInfo = document.createElement('td');
+      allTypeInfo.innerText = combineHolidayType;
+
+      marchTable.appendChild(allRow);
+      allRow.appendChild(allHolidaysInfo);
+      allRow.appendChild(allDateInfo);
+      allRow.appendChild(allTypeInfo);
+      allRow.appendChild(allDescriptInfo);
+
+    } else if (holidays.date.datetime.month == 4) {
+
+      allHolidaysInfo.innerText = holidays.name;
+      allDateInfo.innerText = holidays.date.datetime.month + "/" + holidays.date.datetime.day + "/" + holidays.date.datetime.year
+      allDescriptInfo.innerText = holidays.description;
+
+      const holidayType = holidays.type;
+      const combineHolidayType = holidayType.join(', ');
+      
+      const allTypeInfo = document.createElement('td');
+      allTypeInfo.innerText = combineHolidayType;
+
+      aprilTable.appendChild(allRow);
+      allRow.appendChild(allHolidaysInfo);
+      allRow.appendChild(allDateInfo);
+      allRow.appendChild(allTypeInfo);
+      allRow.appendChild(allDescriptInfo);
+
+    } else if (holidays.date.datetime.month == 5) {
+
+      allHolidaysInfo.innerText = holidays.name;
+      allDateInfo.innerText = holidays.date.datetime.month + "/" + holidays.date.datetime.day + "/" + holidays.date.datetime.year
+      allDescriptInfo.innerText = holidays.description;
+
+      const holidayType = holidays.type;
+      const combineHolidayType = holidayType.join(', ');
+      
+      const allTypeInfo = document.createElement('td');
+      allTypeInfo.innerText = combineHolidayType;
+
+      mayTable.appendChild(allRow);
+      allRow.appendChild(allHolidaysInfo);
+      allRow.appendChild(allDateInfo);
+      allRow.appendChild(allTypeInfo);
+      allRow.appendChild(allDescriptInfo);
+
+    } else if (holidays.date.datetime.month == 6) {
+
+      allHolidaysInfo.innerText = holidays.name;
+      allDateInfo.innerText = holidays.date.datetime.month + "/" + holidays.date.datetime.day + "/" + holidays.date.datetime.year
+      allDescriptInfo.innerText = holidays.description;
+
+      const holidayType = holidays.type;
+      const combineHolidayType = holidayType.join(', ');
+      
+      const allTypeInfo = document.createElement('td');
+      allTypeInfo.innerText = combineHolidayType;
+
+      juneTable.appendChild(allRow);
+      allRow.appendChild(allHolidaysInfo);
+      allRow.appendChild(allDateInfo);
+      allRow.appendChild(allTypeInfo);
+      allRow.appendChild(allDescriptInfo);
+
+    } else if (holidays.date.datetime.month == 7) {
+
+      allHolidaysInfo.innerText = holidays.name;
+      allDateInfo.innerText = holidays.date.datetime.month + "/" + holidays.date.datetime.day + "/" + holidays.date.datetime.year
+      allDescriptInfo.innerText = holidays.description;
+
+      const holidayType = holidays.type;
+      const combineHolidayType = holidayType.join(', ');
+      
+      const allTypeInfo = document.createElement('td');
+      allTypeInfo.innerText = combineHolidayType;
+
+      julyTable.appendChild(allRow);
+      allRow.appendChild(allHolidaysInfo);
+      allRow.appendChild(allDateInfo);
+      allRow.appendChild(allTypeInfo);
+      allRow.appendChild(allDescriptInfo);
+
+    } else if (holidays.date.datetime.month == 8) {
+
+      allHolidaysInfo.innerText = holidays.name;
+      allDateInfo.innerText = holidays.date.datetime.month + "/" + holidays.date.datetime.day + "/" + holidays.date.datetime.year
+      allDescriptInfo.innerText = holidays.description;
+
+      const holidayType = holidays.type;
+      const combineHolidayType = holidayType.join(', ');
+      
+      const allTypeInfo = document.createElement('td');
+      allTypeInfo.innerText = combineHolidayType;
+
+      augustTable.appendChild(allRow);
+      allRow.appendChild(allHolidaysInfo);
+      allRow.appendChild(allDateInfo);
+      allRow.appendChild(allTypeInfo);
+      allRow.appendChild(allDescriptInfo);
+
+    } else if (holidays.date.datetime.month == 9) {
+
+      allHolidaysInfo.innerText = holidays.name;
+      allDateInfo.innerText = holidays.date.datetime.month + "/" + holidays.date.datetime.day + "/" + holidays.date.datetime.year
+      allDescriptInfo.innerText = holidays.description;
+
+      const holidayType = holidays.type;
+      const combineHolidayType = holidayType.join(', ');
+      
+      const allTypeInfo = document.createElement('td');
+      allTypeInfo.innerText = combineHolidayType;
+
+      septemberTable.appendChild(allRow);
+      allRow.appendChild(allHolidaysInfo);
+      allRow.appendChild(allDateInfo);
+      allRow.appendChild(allTypeInfo);
+      allRow.appendChild(allDescriptInfo);
+
+    } else if (holidays.date.datetime.month == 10) {
+
+      allHolidaysInfo.innerText = holidays.name;
+      allDateInfo.innerText = holidays.date.datetime.month + "/" + holidays.date.datetime.day + "/" + holidays.date.datetime.year
+      allDescriptInfo.innerText = holidays.description;
+
+      const holidayType = holidays.type;
+      const combineHolidayType = holidayType.join(', ');
+      
+      const allTypeInfo = document.createElement('td');
+      allTypeInfo.innerText = combineHolidayType;
+
+      octoberTable.appendChild(allRow);
+      allRow.appendChild(allHolidaysInfo);
+      allRow.appendChild(allDateInfo);
+      allRow.appendChild(allTypeInfo);
+      allRow.appendChild(allDescriptInfo);
+
+    } else if (holidays.date.datetime.month == 11) {
+
+      allHolidaysInfo.innerText = holidays.name;
+      allDateInfo.innerText = holidays.date.datetime.month + "/" + holidays.date.datetime.day + "/" + holidays.date.datetime.year
+      allDescriptInfo.innerText = holidays.description;
+
+      const holidayType = holidays.type;
+      const combineHolidayType = holidayType.join(', ');
+      
+      const allTypeInfo = document.createElement('td');
+      allTypeInfo.innerText = combineHolidayType;
+
+      novemberTable.appendChild(allRow);
+      allRow.appendChild(allHolidaysInfo);
+      allRow.appendChild(allDateInfo);
+      allRow.appendChild(allTypeInfo);
+      allRow.appendChild(allDescriptInfo);
+
+    } else if (holidays.date.datetime.month == 12) {
+
+      allHolidaysInfo.innerText = holidays.name;
+      allDateInfo.innerText = holidays.date.datetime.month + "/" + holidays.date.datetime.day + "/" + holidays.date.datetime.year
+      allDescriptInfo.innerText = holidays.description;
+
+      const holidayType = holidays.type;
+      const combineHolidayType = holidayType.join(', ');
+      
+      const allTypeInfo = document.createElement('td');
+      allTypeInfo.innerText = combineHolidayType;
+
+      decemberTable.appendChild(allRow);
+      allRow.appendChild(allHolidaysInfo);
+      allRow.appendChild(allDateInfo);
+      allRow.appendChild(allTypeInfo);
+      allRow.appendChild(allDescriptInfo);
     }
   }
 };
